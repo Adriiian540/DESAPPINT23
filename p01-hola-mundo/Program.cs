dbdd6f41a2489406618154cbad568b37710ddc24
Adriiian540 <adrianrobled0@outlook.com>
2023-09-15 19:43:18 -0600

    p01

---
+++ p01-hola-mundo/Program.cs
@@ -0,0 +1,12 @@
+﻿// Programa que envia un saludo en pantalla
+
+string amigo = "Angel";
+string hermano = "Carlos";
+
+amigo="Osvaldo";
+
+Console.WriteLine("Hola " + amigo);
+Console.WriteLine($"Hola {amigo} bienvenido a c#");
+Console.WriteLine($"\nMi amigo es {amigo} mi hermano es {hermano}");
+Console.WriteLine("Nombres en mayusculas");
+Console.WriteLine($"{amigo.ToUpper()}   {hermano.ToUpper()}");
