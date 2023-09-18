5c2ca3f9076bd376cfa34d6bd80646d286731e33
Adriiian540 <adrianrobled0@outlook.com>
2023-09-17 21:26:55 -0600

    p09

---
+++ p09-volumen-esfera/Program.cs
@@ -0,0 +1,11 @@
+﻿// Dado el radio de un círculo, calcula el volumen de la esfera
+
+double PI = 3.14159;
+double r = 0;
+double v = 0;
+
+Console.Write("Introduce el radio del circulo: "); r = double.Parse(Console.ReadLine());
+
+v = (4.0/3.0)*(Math.Pow(r,3))*(PI);
+
+Console.WriteLine($"El volumen es: {v}");
