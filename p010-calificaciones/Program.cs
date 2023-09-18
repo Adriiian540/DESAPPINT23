f6f3d13328c5bd2091674e47f21dd512181f6d93
Adriiian540 <adrianrobled0@outlook.com>
2023-09-17 21:41:04 -0600

    p010

---
+++ p010-calificaciones/Program.cs
@@ -0,0 +1,35 @@
+﻿// Emitir una calificación con letra y un mensaje de acuerdo al promedio, según la tabla
+
+double calificacion = 0;
+
+Console.Write("Escribe tu nombre: \n"); string nombre = Console.ReadLine();
+
+//do {
+    Console.WriteLine ("Escribe una calificacion entre 0 y 10\n");
+    Console.Write("Introduce tu calificación: "); calificacion = Convert.ToDouble(Console.ReadLine());
+
+switch(calificacion)
+{
+    case <= 5.9: 
+        tabla(nombre);
+        Console.WriteLine($"{calificacion}\t\tD\tAndas mal");
+         break;
+    case <= 7.5 : 
+        tabla(nombre);
+        Console.WriteLine($"{calificacion}\t\tC\tMucho mejor");
+        break;
+    case <= 8.5 : 
+        tabla(nombre);
+        Console.WriteLine($"{calificacion}\t\tB\tMuy bien");
+        break;
+    default:
+        tabla(nombre);
+        Console.WriteLine($"{calificacion}\t\tA\tExelente"); 
+        break;
+}
+
+
+static void tabla(string nombre) {
+        Console.WriteLine($"Alumno: {nombre}");
+        Console.WriteLine("Promedio\tLetra\tMensaje");
+}
